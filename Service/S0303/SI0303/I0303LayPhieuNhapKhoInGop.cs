fa1b9682536784cbd2b8c7f74e43e4ab29cd8044
VoQuiNam <voquinamit@gmail.com>
2025-09-11 00:53:24 +0700

    update

---
+++ Service/S0303/SI0303/I0303LayPhieuNhapKhoInGop.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nam_ThongKeSoLuongBNHenTaiKham.Service.S0303.SI0303
+{
+    public interface I0303LayPhieuNhapKhoInGop
+    {
+        //  Task<IActionResult> ExportToPDF(DateTime ngayGioNhap, int? idChiNhanh,
+        //long? IDKhoHang, long? IDNhaCungCap, long? IDHangHoa, long? IDDonViTinhNhap);
+
+        Task<IActionResult> ExportToPDF(long IdPhieuNhapKhom, int? idChiNhanh);
+    }
+}
