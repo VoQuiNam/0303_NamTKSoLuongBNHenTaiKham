e9032056f8afd11ee81ff0edeb4c5bd7b49dac37
VoQuiNam <voquinamit@gmail.com>
2025-09-12 11:31:26 +0700

    update

--- Models/M0303/M0303BaoCaoThuTongHopDichVuTheoKhoaPhongSTO.cs
+++ Models/M0303/M0303BaoCaoThuTongHopDichVuTheoKhoaPhongSTO.cs
@@ -2,13 +2,25 @@
 {
     public class M0303BaoCaoThuTongHopDichVuTheoKhoaPhongSTO
     {
-        public decimal? Gia { get; set; }
+        //public decimal? Gia { get; set; }
+
+        //public DateTime? NgayTongHop { get; set; }
+
+        //public long? IdPhong { get; set; }
+
+        //public long? IdDichVuKyThuat { get; set; }
+
+        public long? IDNhomDVKT {  get; set; }
+        public string? TenNhomDichVu { get; set; }
+
+        public long? IDDVKT { get; set; }
+        public string? TenDichVu { get; set; }
+        public long? IDPhongBuong { get; set; }
+        public string? TenPhong { get; set; }
+        public double? GiaTien { get; set; }
 
         public DateTime? NgayTongHop { get; set; }
-
-        public long? IdPhong { get; set; }
-
-        public long? IdDichVuKyThuat { get; set; }
+        public long? IdChiNhanh { get; set;  }
 
 
     }
